b9d1fb2a4fa2756890f96db45588d8a70a36d8dc
Manish <kumarmanishparjapati05@gmail.com>
2023-10-01 22:25:49 +0530

    update repo

---
+++ Topics/Basic/Code/Basic.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+void Basic(){
+
+     // Question 1.   Take 2 integer values in two variables x and y and print their product.
+      
+      int x,y,product;
+      cin >> x;
+      cin >> y;
+      product = (x*y);
+      cout<<product<<endl;
+      
+}
